980840fbe0e2cdc86769ad63e3d4ecb4f719f0f9
wills project <williambriandt2018@gmail.com>
2025-02-28 19:25:31 +0000

    my first commit

---
+++ practice.js
@@ -0,0 +1,29 @@
+// let name =prompt("I love Js")
+// console.log("Hello " + name  +  ", be consistent !!!");
+
+// LESSONS LEARNT 
+//. Variables: let name stores an item (message) like a name.
+//. Function: Prompt(), asks for input and console.log() prints messages.
+//. Conditionals: Checks if something is true like checking number.
+
+// let number=Math.floor(Math.random()*10);
+// let guess=prompt("Guess a number 1 - 10 ");
+// if(number == guess) {
+//     console.log("You guess right hurray !!!")
+// } else {
+//     console.log("Oops ! The number was "  +  number  +  ", Try again !");
+//  };
+
+
+
+
+git 
+
+
+
+
+
+
+
+
+
